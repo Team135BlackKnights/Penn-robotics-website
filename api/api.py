8ed16b20fd56971500e57e8b2c33a5a9cbc135ac
AydinK515 <aydink2008@gmail.com>
2024-10-23 18:53:23 -0400

    Starting to test API
    Learning how to use Flask, it is working though API

--- api/api.py
+++ api/api.py
@@ -1,13 +1,21 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
+title = input("What do you want the title to be? ")
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/do-something', methods=['GET'])
 def do_something():
     response = {
-        'message': 'Hello! You requested to do something!'
+        'school_name': title
     }
     return jsonify(response)
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
